e00c3be482183fca3fd796dfe833c62c17c7beaa
Ankur Pandey <pankur216@gmail.com>
2024-08-17 01:46:19 -0400

    Removed boilerplate readme

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,10 @@
+export function CreateTodo() {
+  return (
+    <>
+      <input type="text" placeholder="title"></input>
+      <input type="text" placeholder="description"></input>
+
+      <button>Add a todo</button>
+    </>
+  );
+}
